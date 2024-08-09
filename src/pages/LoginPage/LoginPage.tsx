a9a0f0ca468ae9c2733b62959b58ced1b9422032
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-08-09 15:38:32 -0300

    feat: create show password

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -12,12 +12,16 @@ import { useToast } from "@/components/ui/use-toast";
 import { loginFormData, loginFormSchema } from "@/schemas/Login/login.schema";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
 
 export default function LoginPage() {
   const { login } = useAuth();
 
   const { toast } = useToast()
+
+  const [isVisible, setIsVisible] = useState(false);
 
   const navigate = useNavigate();
 
@@ -54,14 +58,43 @@ export default function LoginPage() {
     <div>
       <form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
         <div className="space-y-2">
-          <TextInput name="taxNumber" type="text" label={"CPF ou CNPJ"} control={control} placeholder="Ex: 999.999.999-99" />
+          <TextInput
+            name="taxNumber"
+            type="text"
+            label={"CPF ou CNPJ"}
+            control={control}
+            placeholder="Ex: 999.999.999-99"
+          />
         </div>
         <div className="space-y-2">
-          <TextInput name="password" type="password" label={"Senha"} control={control} placeholder="Ex: ******"  />
+          <div className="relative">
+            <TextInput
+              name="password"
+              type={isVisible ? "text" : "password"}
+              label={"Senha"}
+              control={control}
+              placeholder="Ex: ******"
+              className="pr-10"
+            />
+            <button
+              type="button"
+              className="absolute inset-y-0 right-0 pr-3 flex pt-5 items-center justify-center h-full w-10"
+              onClick={() => setIsVisible(!isVisible)}
+            >
+              {isVisible ? (
+                <EyeOff size={17} strokeWidth={1.75} />
+              ) : (
+                <Eye size={17} strokeWidth={1.75} />
+              )}
+            </button>
+          </div>
         </div>
         <p className="text-center text-sm text-muted-foreground">
-          Não possui uma conta ainda? 
-          <Link to={'/cadastro'} className="font-medium underline underline-offset-4 hover:text-primary pl-1">
+          Não possui uma conta ainda?
+          <Link
+            to={"/cadastro"}
+            className="font-medium underline underline-offset-4 hover:text-primary pl-1"
+          >
             Cadastrar-se
           </Link>
         </p>
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -4,6 +4,8 @@ import { toast } from "@/components/ui/use-toast";
 import useCreateUser from "@/hooks/useCreateUser";
 import { registerFormData, registerFormSchema } from "@/schemas/Login/register.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
@@ -11,7 +13,9 @@ export default function RegisterPage() {
 
   const { mutateAsync } = useCreateUser();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const [isVisible, setIsVisible] = useState(false);
 
   const { 
     handleSubmit,
@@ -48,16 +52,36 @@ export default function RegisterPage() {
             <TextInput name="name" type="text" label="Nome" control={control} placeholder="Nome do usuário..." />
           </div>
           <div className="grid col-span-6">
-            <TextInput name="password" type="password" label="Senha" control={control} placeholder="Senha..." />
+          <div className="relative">
+            <TextInput
+              name="password"
+              type={isVisible ? "text" : "password"}
+              label={"Senha"}
+              control={control}
+              placeholder="Ex: ******"
+              className="pr-10"
+            />
+            <button
+              type="button"
+              className="absolute inset-y-0 right-0 pr-3 flex pt-5 items-center justify-center h-full w-10"
+              onClick={() => setIsVisible(!isVisible)}
+            >
+              {isVisible ? (
+                <EyeOff size={17} strokeWidth={1.75} />
+              ) : (
+                <Eye size={17} strokeWidth={1.75} />
+              )}
+            </button>
+          </div>
           </div>
           <div className="grid col-span-4">
             <TextInput name="taxNumber" type="text" label="CPF ou CNPJ" control={control} placeholder="###.###.###.##" />
           </div>
           <div className="grid col-span-4">
-            <TextInput name="phone" type="text" label="Telefone" control={control} placeholder="Telefone do usuário..." />
+            <TextInput name="phone" type="text" label="Telefone" control={control} placeholder="Telefone..." />
           </div>
           <div className="grid col-span-4">
-            <TextInput name="mail" type="email" label="E-mail" control={control} placeholder="E-mail do usuário" />
+            <TextInput name="mail" type="email" label="E-mail" control={control} placeholder="E-mail..." />
           </div>
         </div>  
         <Button type="submit" className="w-full">
