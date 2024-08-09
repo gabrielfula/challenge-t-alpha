426fd6bbb01a5e19441665eb8a1586a558527014
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-08-09 07:02:55 -0300

    chore: create edit component product

---
+++ src/components/EditProduct/EditProduct.tsx
@@ -0,0 +1,11 @@
+import { DropdownMenuItem } from '../ui/dropdown-menu'
+
+export default function EditProduct() {
+  return (
+    <>
+      <div>
+        <DropdownMenuItem>Editar</DropdownMenuItem>
+      </div>
+    </>
+  )
+}
